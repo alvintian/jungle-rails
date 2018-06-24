06405c0641e9f2150aff3e8be7bcf93c8cadb67b
alvintian <alvintian2003@yahoo.ca>
2018-06-24 21:41:20 +0000

    changed name/apss to .env

--- app/controllers/concerns/http_auth_concern.rb
+++ app/controllers/concerns/http_auth_concern.rb
@@ -8,7 +8,7 @@ module HttpAuthConcern
         puts Rails.env, "what is Rails.env??"
         return true unless Rails.env == 'development'
         authenticate_or_request_with_http_basic do |username, password|
-            username == 'Jungle' && password == 'book'
+            username == ENV["AUTH_NAME"] && password == ENV["AUTH_PASS"]
         end
     end
 end
